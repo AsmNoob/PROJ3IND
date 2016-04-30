4c940e5ee815493791fd548c2e6dbd927e0752f1
shargaas <gtionogu@ulb.ac.be>
2016-04-30 18:03:16 +0200

    Added Files needed for Algorithm

---
+++ Algorithm/Fampa.py
@@ -0,0 +1,16 @@
+
+
+
+"""
+	This method presents a menu with the different options available
+"""
+def menu():
+
+	print("Welcome")
+
+
+"""
+	This method receives a file with a list of files to parse, then stores all the information in Object
+	usefull for the algorithm.
+"""
+def parsing():
---
+++ Algorithm/Scenario.py
@@ -0,0 +1,4 @@
+class Scenario(object):
+	"""This class stores all the information related to 1 specific scenario."""
+	def __init__(self, arg):
+		self.arg = arg
